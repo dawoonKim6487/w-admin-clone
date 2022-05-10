f3edcaef95f8bbcf8d8a323eb9384123050f1723
kim dawoon <dwkim1@greenlabs.co.kr>
2022-05-10 10:09:26 +0900

    페이징 처리

--- src/components/ui/organisms/board/index.jsx
+++ src/components/ui/organisms/board/index.jsx
@@ -15,45 +15,46 @@ const data = [
     { id: 10, cate: '분류', tit: '제목3', date: '2020-01-01', join: 0 },
     { id: 11, cate: '분류', tit: '제목3', date: '2020-01-01', join: 0 },
 ]
-
+const title = title => {
+    switch (title) {
+        case 'faq':
+            return '자주 묻는 질문'
+        case 'qna':
+            return '묻고 답하기'
+        case 'support':
+            return '공지사항'
+        default:
+            return '게시판'
+    }
+};
 const Board = ({ tit }) => {
-
-    const title = title => {
-        switch (title) {
-            case 'faq':
-                return '자주 묻는 질문'
-            case 'qna':
-                return '묻고 답하기'
-            case 'support':
-                return '공지사항'
-            default:
-                return '게시판'
-        }
+    const dataFilter = (data, page) => {
+        return data.filter((ele) => {
+            return ele.id < (page + 1) * 10 & ele.id >= page * 10
+        })
     }
 
-    const [page] = useState(0);
+    const [page, setPage] = useState(0);
+    const [board, setBoard] = useState(dataFilter(data, page));
 
-    const addPage = async () => {
-        setBoard(data.filter((ele) => {
-            return ele.id < (page + 1) * 10 & ele.id >= page * 10
-        }))
+    const addPage = () => {
+        if (board.length >= 10) {
+            const pageCtrl = page + 1
+            setBoard(dataFilter(data, pageCtrl))
+            setPage(pageCtrl)
+        }
     };
     const minorPage = () => {
-        setBoard(data.filter((ele) => {
-            return ele.id < (page + 1) * 10 & ele.id >= page * 10
-        }))
+        if (page > 0) {
+            const pageCtrl = page - 1
+            setBoard(dataFilter(data, pageCtrl))
+            setPage(pageCtrl)
+        }
     };
-
-    const [board, setBoard] = useState(data.filter((ele) => {
-        return ele.id < (page + 1) * 10 & ele.id >= page * 10
-    }))
-
-
 
     return (
         <div className='px-20 py-10 flex flex-col gap-5 h-full'>
             <BoardTit>{title(tit)}</BoardTit>
-            {page}
             <BoardBody data={data}>
                 {board.map((ele) => <TableRow data={ele} key={ele.id} />)}
             </BoardBody>
